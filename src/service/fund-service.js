4d48784e48606c786fd79430366bf3fcda241a56
Garrett McCullough <garrett.mccullough@clearcapital.com>
2015-07-04 16:16:08 -0700

    mock out backend for test and code service to work

--- src/service/fund-service.js
+++ src/service/fund-service.js
@@ -1,10 +1,34 @@
-angular.module('fundlist').factory('fundService', function() {
+angular
+    .module('fundlist')
+    .factory('fundService', fundService);
 
+fundService.$inject = ['$http', '$log'];
+
+function fundService($http) {
+    var funds = [];
 	var fundService = {
-        getFunds: function() {
-            return 1;
-        }
+        getFunds: getFunds
     };
 
 	return fundService;
-});
+
+    function getFunds() {
+        if (funds.length === 0) {
+            return $http.get('./data/funds.json')
+                .then(getFundsComplete)
+                .catch(getFundsError);
+        } else {
+            return funds;
+        }
+    }
+
+    function getFundsComplete(data) {
+        funds = data.data.funds;
+        //console.log(data.data.funds);
+        return funds;
+    }
+
+    function getFundsError(error) {
+        $log.error('Error retrieving funds.json: ' + error);
+    }
+}
